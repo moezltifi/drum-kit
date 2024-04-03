2878a40c1dbf39c1e599dc69ef4d5c11ce90c276
moezl <moez.mltifi@gmail.com>
2024-04-03 13:50:41 +0100

    make buttons makes sounds same as keys

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 for (let i= 0; i < document.querySelectorAll(".drum").length ; i++) {
+
     
     document.querySelectorAll("button")[i].addEventListener("click", handelclick)
     function handelclick(){
